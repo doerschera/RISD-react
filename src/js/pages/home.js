1e6169e0ccb91bcc4c56786400ff226ffb51b633
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-11-06 13:59:34 -0600

    render arrows

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Arrow from '../components/arrow.js';
 
 export default class Home extends React.Component {
 
   render() {
     return (
-      <div class='valign-wrapper welcome-wrapper'>
-        <h1 class="welcome white-box">Welcome to <span><br></br></span> RISD </h1>
+      <div>
+        <div class='valign-wrapper welcome-wrapper'>
+          <h1 class="welcome white-box">Welcome to <span><br></br></span> RISD </h1>
+        </div>
+        <Arrow />
       </div>
     )
   }
