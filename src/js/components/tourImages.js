c665598da01e0aab5cd954386d01ea802e5df2fb
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-11-07 16:53:26 -0600

    image sizing

--- src/js/components/tourImages.js
+++ src/js/components/tourImages.js
@@ -9,7 +9,9 @@ export default class TourImages extends React.Component {
   render() {
     const stopImages = content[this.props.building].images.map((image, i) =>
       <div class="col m6" key={i}>
-        <img class="responsive-img" src={image} />
+        <div class="image-box">
+          <img class="responsive-img" src={image} />
+        </div>
       </div>
     )
 
