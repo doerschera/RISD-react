e24d65f337b04e5e3da98912184a52e119ff7acd
doerschera <doerschera@gmail.com>
2017-01-30 14:49:53 -0600

    webpack bundle change

---
+++ config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  name: 'dubafz7jf',
+  key: '374813848123594',
+  secret: 'e-w0jo3JRdmPyYzGoqWUsUk89co'
+}
---
+++ public/config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  cloud_name: 'dubafz7jf',
+  cloudinary_key: '374813848123594',
+  cloudinary_secret: 'e-w0jo3JRdmPyYzGoqWUsUk89co'
+}
